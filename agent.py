2877688193a4994f4febe61f95bea4cf163fabda
Clare O'Dwyer <cmsodwyer@gmail.com>
2025-03-31 12:25:51 -0400

    Start implementing agent functions

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ import copy
 import csv
 import pickle
 import re
+import random
 
 class Agent:
     def __init__(self, course_list, episodes, request, epsilon, epsilon_decay, min_epsilon, gamma):
@@ -24,25 +25,59 @@ class Agent:
                         return False
         return True
 
+    # The qtable is a dictionary with keys that are tuples containing ClassNumbers
     def init_qtable(self):
         for course in self.course_list:
-            self.qtable[ course['ClassNumber'] ] = 0.0
+            self.qtable[ (course['ClassNumber']) ] = 0.0
 
+    # Provide a list of all possible actions
     def get_all_possible_actions(self, state):
-        # implement
-        # provide a list of all possible actions
+        all_actions = []
+
+        # If the last course selected has required labs/ discussions, force the agent to select one
+        if state[-1]['RequiredSections'] :
+            for course in state[-1]['RequiredSections']:
+                all_actions.append(course['ClassNumber'])
+            return all_actions
+
+        # Otherwise, return all courses not already in the schedule
+        for course in self.course_list:
+            if course['ClassNumber'] not in state:
+                all_actions.append(course['ClassNumber'])
+
+        all_actions.append('STOP')
+        return all_actions
 
     def get_action(self, state):
-        # implement
         # using epilson greedy, pick the current best or random action
+        true_epsilon = max(self.min_epsilon, self.epsilon)
+        random_decimal = random.random()
+        actions = self.get_all_possible_actions(state)
+
+        if (random_decimal < true_epsilon):
+            # Select random action
+            random_action = random.choice(actions)
+            return random_action
+        else: # Select best action
+            best_action = actions[0]
+            state_tuple = tuple(state)
+            max_qvalue = self.qtable[state_tuple + tuple(best_action)]
+
+            for action in actions:
+                qvalue = self.qtable[state_tuple + tuple(action)]
+                if qvalue > max_qvalue:
+                    max_qvalue = qvalue
+                    best_action = action
+            return best_action
 
     def step(self, state, action):
         # implement
         # return next_state, reward
 
     def get_reward(self, request, state, action):
-        # implement
         # return reward
+        # placeholder
+        return 5
 
     def update_qtable(self, old_state, action, reward, next_state):
         # implement
@@ -51,6 +86,10 @@ class Agent:
     # Return the total number of credits in the current state
     def get_num_credits(self, state):
         #implement
+        num_credits = 0
+        for course in state:
+            num_credits += int(course['Units'])
+        return num_credits
 
     # Training function based off of HW4 RL
     def train(self):
@@ -60,14 +99,17 @@ class Agent:
                 state = []
                 action = self.get_action(state)
 
-                if action == 'STOP' or self.get_num_credits(state) > 23:
+                if action == 'STOP' or self.get_num_credits(state) > 25: # Force the agent to stop after 25 credits
                     condition = 'Done'
 
                 next_state, reward = self.step(state, action)
 
                 old_state = copy.deepcopy(state)
                 self.qtable = self.update_qtable(old_state, action, reward, next_state)
+            # Update the epsilon value
+            self.epsilon = max(self.min_epsilon, self.epsilon * self.epsilon_decay)
 
+        # Write the qtable to a file
         with open("","wb") as f:
             pickle.dump(self.qtable, f)
         print("Training complete! Q-table saved")
