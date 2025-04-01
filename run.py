cc1cf82dd04625110234e8bfefc9b76734a0d934
Clare O'Dwyer <cmsodwyer@gmail.com>
2025-03-31 23:18:41 -0400

    Training on smaller set

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ def main():
     episodes = 10000
 
     parser = Parser()
-    course_list = parser.parse_csv_into_dict('classes.csv')
+    course_list = parser.parse_csv_into_dict('classes2.csv')
     course_list = parser.remove_extra_courses(course_list)
     parser.parse_times(course_list)
     course_list = parser.consolidate_lectures_and_discussions(course_list)
@@ -13,7 +13,8 @@ def main():
     # for course in course_list:
     #     print(course)
 
-    request = {'DesiredCourses': [{'Mnemonic': 'CS', 'Number': '7457'}, {'Mnemonic': 'CS', 'Number': '3205'}],
+    request = {'DesiredCourses': [{'Mnemonic': 'CS', 'Number': '3250'}, {'Mnemonic': 'CS', 'Number': '4710'},
+                                  ],
                'MaxCredits': 19,
                'MinCredits': 14,
                'Keywords': ['human', 'database', 'design', 'system']
@@ -21,13 +22,14 @@ def main():
 
     epsilon = 1.0
     min_epsilon = .01
-    epsilon_decay = .99
+    epsilon_decay = .999
     gamma = .95
-    alpha = 0.2
+    alpha = .9
 
     agent = Agent(course_list, episodes, request, epsilon, epsilon_decay, min_epsilon, gamma, alpha)
     agent.init_qtable()
     agent.train()
+    agent.find_best_schedule()
 
     # for q in agent.qtable:
     #     print(q, agent.qtable[q])
